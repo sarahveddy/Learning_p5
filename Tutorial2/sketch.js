3060bff2b8b0b74c8eb1a59df02b91dcc7c1e7d8
Sarah Vedress <>
2018-04-16 17:08:03 -0600

    Starting second tutorial drawing line

--- Tutorial2/sketch.js
+++ Tutorial2/sketch.js
@@ -1,22 +1,12 @@
 function setup() {
   // put setup code here
-  createCanvas(640, 480);
-  //create window (width, height)
+  line(15, 25, 70, 90);
+  //(x1, y1, x2, y2)
 
 }
 
 function draw() {
   // put drawing code here
-  //ellipse(50, 50, 80, 80);
-  //(center x, center y, width, height)
-  if(mouseIsPressed){
-  	fill(0);
-  	//fill colour is black
-  }
-  else{
-  	fill(225);
-  	//fill colour is white
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+ 
 
 }
