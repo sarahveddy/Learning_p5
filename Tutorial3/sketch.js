60ea9a8819edcec3b7faeb1ba7b02ae1e5cfff37
Sarah Vedress <>
2018-05-02 11:30:23 -0600

    Creating HTML images

--- Tutorial3/sketch.js
+++ Tutorial3/sketch.js
@@ -1,32 +1,28 @@
+var img;
+var canvas;
+
+
+
 function setup(){
-  //store canvas pointer in a variable
-  myCanvas = createCanvas(600, 400);
-  
-  //change the position of the canvas and give it a class
-  myCanvas.position(300, 50);
-  // position() overrides the default positioning of the element
-  //by applying a CSS style position:absolute
+  canvas = createCanvas(400, 400);
 
-  myCanvas.class("lemon");
-  //this element holds a static state, and can be changed by calling its methods
-  
-  //make the canvas a child of an element with id 'myContainer'
-  myCanvas.parent('myContainer');
-
-  //creates a new div
-  txt = createDiv('HTML string');
-  txt.position(50, 50);
-
-
-
-
+  //creates an html image with src "https:..."
+  //unlike using image(), this doesn't have to be loaded or drawn every frame in draw()
+  img = createImg("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg");
+  img.position(190, 50);
+  img.size(200, 200);
+  canvas.position(300, 50);
 }
 
 function draw(){
-  background(220, 180, 200);
-  ellipse(width/2, height/2, 100, 100);
-  ellipse(width/4, height/2, 50, 50);
-  //these elements are redrawn several times per second 
+  noStroke();
+  background(color('blue'));
+  //fill(color("yellow"));
+  strokeWeight(6);
+  stroke(180, 100, 240);
+  for(var i = 0; i < width; i += 15){
+    line(i, 0, i, height);
+  }
 }
 
 
