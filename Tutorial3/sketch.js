de80b0fedb2dd2029c40f37c28181c4f11f7b1b1
Sarah Vedress <>
2018-05-02 16:05:36 -0600

    great example of global and element specific listeners

--- Tutorial3/sketch.js
+++ Tutorial3/sketch.js
@@ -1,45 +1,25 @@
-var img;
-var canvas;
+var gray = 0;
+var diameter = 5;
 
 function setup(){
-  canvas = createCanvas(400, 400);
-  img = createImg('https://vignette.wikia.nocookie.net/studio-ghibli/images/5/53/Totoro.jpg/revision/latest?cb=20180107143036');
-  img.position(190, 50);
-  img.size(200, 150);
-  canvas.position(300, 50);
-  img.mousePressed(uniHide);
-
-  //2 ways of doing a mouse event, one with an anonymous function and one without
-  // canvas.mouseOver(uniHide);
-  // canvas.mouseOut(function(){
-  //   img.show();
-  // });
+  var canvas =  createCanvas(200, 200);
+  
+  //mousepressed() takes a function
+  //handles mouse presses in the canvas only
+  canvas.mousePressed(incDiameter);
 }
 
-function draw(){
-  noStroke();
-  background(220, 180, 200);
-  fill(200, 200, 40);
-  strokeWeight(6);
-  stroke(180, 100, 240);
-  for (var i = 0; i < width; i += 15){
-    line( i, 0, i, height);
-  }
-  if (mouseIsPressed){
-    ellipse(mouseX, mouseY, 5, 5);
-  }
+function draw() {
+  background(gray);
+  ellipse(width/2, height/2, diameter, diameter);
 }
 
-function uniHide(){
-  img.hide();
+//handles mouse presses in the entire window (including the canvas)
+function mousePressed() {
+  gray = gray + 10;
 }
 
-function uniShow(){
-  img.show();
-}
-
-//global function for keyPress
-function keyPressed(){
-  uniShow();
+function incDiameter() {
+  diameter = diameter + 5;
 }
 
