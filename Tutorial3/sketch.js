46fad25965548d2dec22dbf7add8fec2da374fe3
Sarah Vedress <>
2018-05-02 16:01:30 -0600

    more mouse events, global and element specific

--- Tutorial3/sketch.js
+++ Tutorial3/sketch.js
@@ -3,27 +3,30 @@ var canvas;
 
 function setup(){
   canvas = createCanvas(400, 400);
-  img = createImg('https://vignette.wikia.nocookie.net/studio-ghibli/images/5/53/Totoro.jpg/revision/latest?cb=20180107143036')
-
+  img = createImg('https://vignette.wikia.nocookie.net/studio-ghibli/images/5/53/Totoro.jpg/revision/latest?cb=20180107143036');
   img.position(190, 50);
   img.size(200, 150);
-
   canvas.position(300, 50);
-  canvas.mouseOver(uniHide);
-  //
-  canvas.mouseOut(function(){
-    img.show();
-  });
+  img.mousePressed(uniHide);
+
+  //2 ways of doing a mouse event, one with an anonymous function and one without
+  // canvas.mouseOver(uniHide);
+  // canvas.mouseOut(function(){
+  //   img.show();
+  // });
 }
 
 function draw(){
   noStroke();
   background(220, 180, 200);
-  fill(180, 200, 40);
-  strokeweight(6);
+  fill(200, 200, 40);
+  strokeWeight(6);
   stroke(180, 100, 240);
   for (var i = 0; i < width; i += 15){
     line( i, 0, i, height);
+  }
+  if (mouseIsPressed){
+    ellipse(mouseX, mouseY, 5, 5);
   }
 }
 
@@ -31,6 +34,12 @@ function uniHide(){
   img.hide();
 }
 
+function uniShow(){
+  img.show();
+}
 
-
+//global function for keyPress
+function keyPressed(){
+  uniShow();
+}
 
