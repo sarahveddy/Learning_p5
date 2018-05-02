2a027979444ccf70b6ccea4ebf746c6f91d5d829
Sarah Vedress <>
2018-05-02 11:56:15 -0600

    Working with videos

--- Tutorial3/sketch.js
+++ Tutorial3/sketch.js
@@ -1,29 +1,35 @@
-var img;
-var canvas;
-
-
+var playing = false;
+var video; 
+var playButton;
 
 function setup(){
-  canvas = createCanvas(400, 400);
+  //creates an html video element, and html button element and attaches a 
+  //element specific event listener to the button
+  video = createVideo('Resources/mouse.mov');
+  button = createButton("Play Video!");
+  //mousePressed takes a function as an argument
+  button.mousePressed(toggleVid); 
+}
 
-  //creates an html image with src "https:..."
-  //unlike using image(), this doesn't have to be loaded or drawn every frame in draw()
-  img = createImg("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg");
-  img.position(190, 50);
-  img.size(200, 200);
-  canvas.position(300, 50);
+function toggleVid(){
+  if(playing){
+    //pauses vid
+    video.pause();
+    //changes words on button
+    button.html('play');
+  }
+  else{
+    //allows the video to loop
+    video.loop();
+    button.html('pause');
+  }
+  playing = !playing;
 }
 
 function draw(){
-  noStroke();
-  background(color('blue'));
-  //fill(color("yellow"));
-  strokeWeight(6);
-  stroke(180, 100, 240);
-  for(var i = 0; i < width; i += 15){
-    line(i, 0, i, height);
-  }
+  
 }
+
 
 
 
