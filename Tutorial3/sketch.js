d5cae07225f472ae1b52f66748b541d09b12fb87
Sarah Vedress <>
2018-05-02 10:48:44 -0600

    Storing pointers and calling methods

--- Tutorial3/sketch.js
+++ Tutorial3/sketch.js
@@ -1,6 +1,20 @@
+function setup(){
+  //store canvas pointer in a variable
+  myCanvas = createCanvas(600, 400);
+  //change the position of the canvas and gives it a class
+  myCanvas.position(300, 50);
+  myCanvas.class("lemon");
+  //this element holds a static state, and can be changed by calling its methods
 
 
+}
 
+function draw(){
+  background(220, 180, 200);
+  ellipse(width/2, height/2, 100, 100);
+  ellipse(width/4, height/2, 50, 50);
+  //these elements are redrawn several times per second 
+}
 
 
 
